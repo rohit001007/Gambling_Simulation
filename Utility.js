90837c1d58341358dea3eb4df440995c1cab9503
rohit001007 <rohit001007@gmail.com>
2020-12-25 16:21:39 +0530

    Refactor : Update The Win Or Lose Code

--- Utility.js
+++ Utility.js
@@ -2,25 +2,23 @@ class Utility{
 
     dailyMergin = 100;
     everyDayBet = 1;
-    Win = 0;
-    Lose = 0;
-
-    gamble_WinORLose = () => {
-        return Math.floor((Math.random()*1)+1);
-    }    
 
     check_WinORLose = () => {
+        return Math.floor(Math.random()* 2);
+    }    
 
-        let Result = this.gamble_WinORLose();
+    main_Simulation = () => {
 
-        if(Result == 1) {
-            this.Win += 1;
-            this.dailyMergin += 1;
-        }
-        if (Result == 0) {
-            this.Lose += 1;
-            this.dailyMergin -= 1;
-        }
+        let Result = this.check_WinORLose();
+
+            if(Result == 1) {
+                this.dailyMergin += 1;
+                console.log(`\n * GAMBLER WIN *`)
+            }
+            if(Result == 0) {
+                this.dailyMergin -= 1;
+                console.log(`\n * GAMBLER LOSE *`)
+            }
     }
 }
 
